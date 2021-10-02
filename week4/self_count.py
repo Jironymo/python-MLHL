a13d7ef070c5e154d74794f8dc2abfd4120d9e02
jironymo jirolamus <greedisneutral@gmail.com>
2021-10-02 04:17:54 +0300

    finished week4 hw

---
+++ week4/self_count.py
@@ -0,0 +1,48 @@
+from typing import Any
+
+
+class SelfCount:
+    count = 0
+    
+    @classmethod
+    def increment_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def decrement_count(cls):
+        cls.count -= 1
+    
+
+    def __init__(self):
+        SelfCount.increment_count()
+
+    def __getattribute__(self, name: str):
+        if name == 'count':
+            return SelfCount.count
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == 'count':
+            return None
+
+    def __delattr__(self, name: str) -> None:
+        if name == 'count':
+            return None
+
+    def __del__(self):
+        SelfCount.decrement_count()
+
+
+
+a = SelfCount()
+b = SelfCount()
+c = SelfCount()
+a.count = 2
+del a.count
+
+print(a.count)
+print(b.count)
+print(c.count)
+
+del a
+print(c.count)
+
---
+++ week4/sizer.py
@@ -0,0 +1,30 @@
+def sizer(cls):
+
+    class Wrapper:
+
+        def __init__(self, *args, **kwargs):
+            self._obj = cls(*args, **kwargs)
+            
+            try:
+                self.size = len(self._obj)
+            except TypeError:
+                self.size = abs(int(self._obj))
+
+        def __repr__(self):
+            return str(self._obj) 
+
+    return Wrapper
+
+if __name__ == '__main__'
+@sizer
+class S(str): pass
+
+@sizer
+class N(float): pass
+
+s = S("QSXWDC")
+n = N(2.718281828459045)
+print(s, n)
+print(s.size, n.size)
+s.size, n.size = "Wait", "what?"
+print(s.size, n.size) 
